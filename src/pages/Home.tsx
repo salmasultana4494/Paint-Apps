f869e993e380a80b401c1ec01a7bbe523ef67c5e
salmasultana4494 <salmasultana4494@gmail.com>
2024-01-25 12:30:08 +0600

    google login

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,42 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonText, IonRow, IonCol, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonImg } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar color="primary">
+            <IonTitle>Ionic React App</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+        <IonContent className="ion-padding">
+          <IonRow>
+            <IonCol className="text-center">
+              <IonImg className="title-img" src="assets/capacitor.png" ></IonImg>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol className="text-center">
+              <IonText className="title">
+                Google Login in Capacitor app
+              </IonText>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol className="text-center">
+              <IonText className="text-center">
+                By Enappd Team
+              </IonText>
+            </IonCol>
+          </IonRow>
+
+          <IonButton className="login-button" expand="block" fill="solid" color="danger">
+            Login with Google
+        </IonButton>
+        </IonContent>
+      </IonPage>
+    )
 };
 
 export default Home;
